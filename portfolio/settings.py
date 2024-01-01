968fa6724cb5bb096cd5dd33b26060f691c11679
shantanuchavhan <shantanuchavhan10@gmail.com>
2024-01-01 18:20:54 +0530

    commit

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG =os.environ.get("DEBUG","False").lower()=="true"
 # 
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS")
+ALLOWED_HOSTS =os.environ.get("ALLOWED_HOSTS").split(" ")
 #  ["portfolioapp-q7ra.onrender.com ", "127.0.0.1:8000",  "127.0.0.1"]
 
 
