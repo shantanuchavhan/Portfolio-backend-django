c48b469270c7cc7877f3731053dfad6c31977f1f
shantanuchavhan <shantanuchavhan10@gmail.com>
2024-01-01 17:09:56 +0530

    commit

--- api/models.py
+++ api/models.py
@@ -7,8 +7,8 @@ import cloudinary.models
 class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    images = models.ImageField(upload_to='project_images/', null=True, blank=True)
-    videos = models.FileField(upload_to='project_videos/', null=True, blank=True)
+    images = cloudinary.models.CloudinaryField('images', null=True, blank=True)
+    videos = cloudinary.models.CloudinaryField('videos', null=True, blank=True)
 
     def __str__(self):
         return self.title
