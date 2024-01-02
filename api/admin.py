58bf38e26a2d6892261e509454773e7c35a3a23d
shantanuchavhan <shantanuchavhan10@gmail.com>
2024-01-02 14:26:47 +0530

    commit

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Project, ContactSubmission
+from .models import Project, ContactSubmission,Category
 
 admin.site.register(Project)
+admin.site.register(Category)
 admin.site.register(ContactSubmission)
